d6b2f1cfd51b1d9eb84bd209ba56f97ebaf575bd
Artem <artiebears@mail.ru>
2024-12-03 15:21:21 +0300

    fix debounce

--- react-chat/src/components/SearchBar/SearchBar.jsx
+++ react-chat/src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,14 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState, useMemo } from 'react';
 import styles from './SearchBar.module.scss';
 import SearchIcon from '@mui/icons-material/Search';
+import {debounce} from "../../utils/debounce.js";
 
-export const SearchBar = ({onSearch, isSearchOpen, setSearchOpen}) => {
-    const [isAnimating, setIsAnimating] = useState(false); // Для контроля анимации
+export const SearchBar = ({ onSearch, isSearchOpen, setSearchOpen }) => {
+    const [isAnimating, setIsAnimating] = useState(false);
     const [searchQuery, setSearchQuery] = useState('');
     const inputRef = useRef(null);
-    const debounceTimer = useRef(null);
+
+    const debouncedOnSearch = useMemo(() => debounce(onSearch, 300), [onSearch]);
 
     useEffect(() => {
         const escapeHandler = (e) => {
@@ -34,15 +36,7 @@ export const SearchBar = ({onSearch, isSearchOpen, setSearchOpen}) => {
         const query = e.target.value;
         setSearchQuery(query);
 
-        // Очистка предыдущего таймера
-        if (debounceTimer.current) {
-            clearTimeout(debounceTimer.current);
-        }
-
-        // Установка нового таймера
-        debounceTimer.current = setTimeout(() => {
-            onSearch(query);
-        }, 300); // Задержка 300 мс
+        debouncedOnSearch(query);
     };
 
     const handleSearchInputBlur = () => {
@@ -55,7 +49,7 @@ export const SearchBar = ({onSearch, isSearchOpen, setSearchOpen}) => {
             setSearchOpen(false);
             setIsAnimating(false);
             setSearchQuery('');
-            onSearch(''); // Сбрасываем результат поиска
+            onSearch('');
         }, 300);
     };
 
@@ -66,7 +60,7 @@ export const SearchBar = ({onSearch, isSearchOpen, setSearchOpen}) => {
                     className={`${styles.searchButton} ${styles.white}`}
                     onClick={handleSearchIconClick}
                 >
-                    <SearchIcon/>
+                    <SearchIcon />
                 </button>
             )}
 
@@ -75,7 +69,8 @@ export const SearchBar = ({onSearch, isSearchOpen, setSearchOpen}) => {
                     ref={inputRef}
                     type="text"
                     placeholder="Поиск..."
-                    className={`${styles.searchInput} ${isSearchOpen ? styles.scaleInHorRight : ''} ${isAnimating ? styles.scaleOutHorRight : ''}`}
+                    className={`${styles.searchInput} ${isSearchOpen ? styles.scaleInHorRight : ''
+                    } ${isAnimating ? styles.scaleOutHorRight : ''}`}
                     value={searchQuery}
                     onChange={handleSearchInputChange}
                     onBlur={handleSearchInputBlur}
