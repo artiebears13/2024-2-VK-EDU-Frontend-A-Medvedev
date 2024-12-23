a3a2f5797a57b0be1f2ff8a105802ad5f321ff1a
Artem <artiebears@mail.ru>
2024-12-23 21:21:58 +0300

    fixed package

--- exam/src/main.tsx
+++ exam/src/main.tsx
@@ -4,14 +4,11 @@ import './index.css'
 import App from './App.tsx'
 import {Provider} from "react-redux";
 import store from "./store";
-import {HashRouter as Router} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Provider store={store}>
-            <Router>
-                <App/>
-            </Router>
+            <App/>
         </Provider>
     </StrictMode>,
 )
