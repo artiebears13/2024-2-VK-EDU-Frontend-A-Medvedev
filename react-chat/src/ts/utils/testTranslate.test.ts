f7ef34013397bc5906e3bfc0b640fd4273f43065
Artem <artiebears@mail.ru>
2025-01-22 17:10:44 +0300

    add mocks for cache test

--- react-chat/src/ts/utils/testTranslate.test.ts
+++ react-chat/src/ts/utils/testTranslate.test.ts
@@ -1,40 +1,49 @@
 import { Translator } from './class';
 import { fetchTranslation } from './translate';
 
-describe('Translator Integration Test', () => {
+describe('Translator Test', () => {
     let translator: Translator;
 
     beforeEach(() => {
         translator = new Translator('ru', 'en');
+        jest.restoreAllMocks();
     });
 
-    test('Check latin symbols in translation', async () => {
-        const result = await translator.translate("Привет, мир");
+    test('Integration test', async () => {
+
+        let result;
+        result = await translator.translate("Привет, мир");
         expect(/[A-Za-z]/.test(result.translatedText)).toBe(true);
-    });
 
-    test('Test cache', async () => {
-        await translator.translate("Привет, мир");
-        const result = await translator.translate("Привет, мир");
-        expect(result.usedCache).toBe(true);
-    });
-
-    test('Test Cyrillic symbols in translation', async () => {
         translator.changeFromLang('en');
         translator.changeToLang('ru');
-        const result = await translator.translate("hello, world");
+        result = await translator.translate("hello, world");
         expect(/[А-Яа-я]/.test(result.translatedText)).toBe(true);
     });
 
-    test('Test cache', async () => {
-        const testData = {
-            text: "Hello, world!",
-            fromLanguage: "en",
-            toLanguage: "ru",
-        };
-        await fetchTranslation(testData);
-        const result = await fetchTranslation(testData);
-        expect(result.usedCache).toBe(true);
-    });
+    test('Cache test', async () => {
+        const fetchSpy = jest.spyOn(global, 'fetch');
 
+        fetchSpy.mockResolvedValueOnce(new Response(JSON.stringify({
+            responseData: { translatedText: 'Hello, world!', match: 1},
+            responseStatus: 200
+        }), {status: 200}));
+
+        const testString = "Привет, мир";
+        let result;
+
+        result = await translator.translate(testString);
+        expect(result.usedCache).toBe(false);
+
+        result = await translator.translate(testString);
+        expect(result.usedCache).toBe(true);
+
+        result = await translator.translate(testString + " что то еще");
+        expect(result.usedCache).toBe(false);
+
+        translator.changeFromLang('en');
+        translator.changeToLang('ru');
+        result = await translator.translate(testString);
+        expect(result.usedCache).toBe(false);
+    });
 });
